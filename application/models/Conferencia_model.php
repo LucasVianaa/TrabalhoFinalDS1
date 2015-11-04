2b0157970df5da8ceea995d3109e6085f8913798
Razue <lucas.viana.rs@hotmail.com>
2015-11-04 15:57:06 -0200

    Model/Fix na build do banco

--- application/models/Conferencia_model.php
+++ application/models/Conferencia_model.php
@@ -12,5 +12,105 @@
  * @author aluno
  */
 class Conferencia_model extends CI_Model{
-    //put your code here
+    private $codigo, $cidade, $precoEstudante, $precoProfessor, $precoOutros,
+            $pais, $dataInicio, $dataFim, $topicosInteresse, $areaConhecimento,
+            $dataInicioInscricao, $dataFimInscricao;
+    
+    function getCodigo() {
+        return $this->codigo;
+    }
+
+    function getCidade() {
+        return $this->cidade;
+    }
+
+    function getPrecoEstudante() {
+        return $this->precoEstudante;
+    }
+
+    function getPrecoProfessor() {
+        return $this->precoProfessor;
+    }
+
+    function getPrecoOutros() {
+        return $this->precoOutros;
+    }
+
+    function getPais() {
+        return $this->pais;
+    }
+
+    function getDataInicio() {
+        return $this->dataInicio;
+    }
+
+    function getDataFim() {
+        return $this->dataFim;
+    }
+
+    function getTopicosInteresse() {
+        return $this->topicosInteresse;
+    }
+
+    function getAreaConhecimento() {
+        return $this->areaConhecimento;
+    }
+
+    function getDataInicioInscricao() {
+        return $this->dataInicioInscricao;
+    }
+
+    function getDataFimInscricao() {
+        return $this->dataFimInscricao;
+    }
+
+    function setCodigo($codigo) {
+        $this->codigo = $codigo;
+    }
+
+    function setCidade($cidade) {
+        $this->cidade = $cidade;
+    }
+
+    function setPrecoEstudante($precoEstudante) {
+        $this->precoEstudante = $precoEstudante;
+    }
+
+    function setPrecoProfessor($precoProfessor) {
+        $this->precoProfessor = $precoProfessor;
+    }
+
+    function setPrecoOutros($precoOutros) {
+        $this->precoOutros = $precoOutros;
+    }
+
+    function setPais($pais) {
+        $this->pais = $pais;
+    }
+
+    function setDataInicio($dataInicio) {
+        $this->dataInicio = $dataInicio;
+    }
+
+    function setDataFim($dataFim) {
+        $this->dataFim = $dataFim;
+    }
+
+    function setTopicosInteresse($topicosInteresse) {
+        $this->topicosInteresse = $topicosInteresse;
+    }
+
+    function setAreaConhecimento($areaConhecimento) {
+        $this->areaConhecimento = $areaConhecimento;
+    }
+
+    function setDataInicioInscricao($dataInicioInscricao) {
+        $this->dataInicioInscricao = $dataInicioInscricao;
+    }
+
+    function setDataFimInscricao($dataFimInscricao) {
+        $this->dataFimInscricao = $dataFimInscricao;
+    }
+
+
 }
--- application/models/Usuario_model.php
+++ application/models/Usuario_model.php
@@ -12,5 +12,105 @@
  * @author aluno
  */
 class Usuario_model extends CI_Model{
-    //put your code here
+    private $codigo, $nome, $login, $senha, $dataNascimento, $endereco, $telefone,
+            $cpf, $email, $instituicao, $conferencias, $areasConhecimento;
+    
+    function getAreasConhecimento() {
+        return $this->areasConhecimento;
+    }
+
+    function setAreasConhecimento($areasConhecimento) {
+        $this->areasConhecimento = $areasConhecimento;
+    }
+
+        
+    function getCodigo() {
+        return $this->codigo;
+    }
+
+    function getNome() {
+        return $this->nome;
+    }
+
+    function getLogin() {
+        return $this->login;
+    }
+
+    function getSenha() {
+        return $this->senha;
+    }
+
+    function getDataNascimento() {
+        return $this->dataNascimento;
+    }
+
+    function getEndereco() {
+        return $this->endereco;
+    }
+
+    function getTelefone() {
+        return $this->telefone;
+    }
+
+    function getCpf() {
+        return $this->cpf;
+    }
+
+    function getEmail() {
+        return $this->email;
+    }
+
+    function getInstituicao() {
+        return $this->instituicao;
+    }
+
+    function getConferencias() {
+        return $this->conferencias;
+    }
+
+    function setCodigo($codigo) {
+        $this->codigo = $codigo;
+    }
+
+    function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    function setLogin($login) {
+        $this->login = $login;
+    }
+
+    function setSenha($senha) {
+        $this->senha = $senha;
+    }
+
+    function setDataNascimento($dataNascimento) {
+        $this->dataNascimento = $dataNascimento;
+    }
+
+    function setEndereco($endereco) {
+        $this->endereco = $endereco;
+    }
+
+    function setTelefone($telefone) {
+        $this->telefone = $telefone;
+    }
+
+    function setCpf($cpf) {
+        $this->cpf = $cpf;
+    }
+
+    function setEmail($email) {
+        $this->email = $email;
+    }
+
+    function setInstituicao($instituicao) {
+        $this->instituicao = $instituicao;
+    }
+
+    function setConferencias($conferencias) {
+        $this->conferencias = $conferencias;
+    }
+
+
 }
